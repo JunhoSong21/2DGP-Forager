ceda009fe05a9ce736e50d4e2bf27a9b858b887b
JunhoSong21 <junhoasd1zxc2@gmail.com>
2024-12-02 16:44:03 +0900

    백그라운드 도트찍어서 이미지로 사용하기로 결정

--- background.py
+++ background.py
@@ -2,4 +2,13 @@ import game_framework
 import pico2d
 
 class BackGround:
-    pass
+    image = None
+
+    def __init__(self):
+        self.x, self.y = 960, 540
+    
+    def draw(self):
+        self.image.draw()
+
+    def update(self):
+        pass
