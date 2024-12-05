3072364382f757758bc895c29b5b4010a850a5d3
JunhoSong21 <164911108+JunhoSong21@users.noreply.github.com>
2024-12-05 22:46:28 +0900

    메인메뉴 음악 구현

--- background.py
+++ background.py
@@ -9,7 +9,7 @@ class BackGround:
         if BackGround.image == None:
             BackGround.image = pico2d.load_image('Sprites/BackGroundLight.png')
 
-        self.bgm = pico2d.load_wav('Sounds/Forest2.wav')
+        self.bgm = pico2d.load_wav('Sounds/Forest1.wav')
         self.bgm.set_volume(32)
         self.bgm.repeat_play()
     
--- mainmenuplay.py
+++ mainmenuplay.py
@@ -9,6 +9,10 @@ class MainMenuPlay:
         if MainMenuPlay.image == None:
             MainMenuPlay.image = pico2d.load_image('Sprites/MainMenuPlay.png')
 
+        self.bgm = pico2d.load_wav('Sounds/Forest2.wav')
+        self.bgm.set_volume(32)
+        self.bgm.repeat_play()
+
     def draw(self):
         self.image.clip_draw(0, 0, 104, 33, self.x, self.y, 624, 198)
 
