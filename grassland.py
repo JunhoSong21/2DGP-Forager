9b3a53155b68d66b15620262db7dab5248a85c97
JunhoSong21 <164911108+JunhoSong21@users.noreply.github.com>
2024-12-07 02:11:03 +0900

    땅 변경 및 적용

--- grassland.py
+++ grassland.py
@@ -4,7 +4,7 @@ from forager import *
 
 class GrassLand:
     def __init__(self):
-        self.image = pico2d.load_image('Sprites/GrassLand.png')    
+        self.image = pico2d.load_image('Sprites/GrassLandSquare.png')    
 
     def draw(self):
         self.image.draw(1920 - server.forager.x, 1080 - server.forager.y, 560, 588)
